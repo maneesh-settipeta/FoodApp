f49931b989af430cda0a819b56a5e3a5d0d2972c
mahenv <mahendrauppari044@gmail.com>
2024-07-06 20:16:23 -0400

    updated the modal logic

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,27 +1,40 @@
-import Modal from "./UI/Button";
-import { useContext } from "react";
+import Modal from "./UI/Modal";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../Store/CartContext";
 import UserProgressContext from "../Store/UserProgressContext";
 function Cart() {
   const { items } = useContext(CartContext);
-  const { userprogress } = useContext(UserProgressContext);
+  const { userprogress, hideCart } = useContext(UserProgressContext);
   const totalCount = items.reduce(
     (totalCoutItems, item) => totalCoutItems + item.quantity * item.price,
     0
   );
-  console.log(userprogress);
 
   return (
     <Modal open={userprogress === "cart"}>
-      <h2>Your Items</h2>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            {item.name}-{item.quantity}
-          </li>
-        ))}
-      </ul>
-      <p>Total{totalCount}</p>
+      <div className="dialog">
+        <h2>Your Items</h2>
+        <div>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                {item.name}-{item.quantity}
+              </li>
+            ))}
+          </ul>
+          <p>Total{totalCount}</p>
+        </div>
+        <div className="modal-footer">
+          <button
+            className="closeBtn"
+            onClick={() => {
+              hideCart();
+            }}
+          >
+            Close
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 }
